5dad33ec38269c402f652ffc55570e67119c584d
NetLord8 <ilhom.ulmasov@gmail.com>
2016-05-16 11:57:31 +0300

    Starting to learn maven, git and spring

---
+++ src/com/tutorialspoint/MainApp.java
@@ -0,0 +1,20 @@
+package com.tutorialspoint;
+
+/**
+ * Created by ulmasov_im on 16.05.2016.
+ */
+
+
+        import org.springframework.context.ApplicationContext;
+        import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("Beans.xml");
+
+        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+
+        obj.getMessage();
+    }
+}
